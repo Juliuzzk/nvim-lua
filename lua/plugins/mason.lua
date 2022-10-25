7dd5dbc1e61691ac70fa38dbf3b6479683be5571
Julio Cáceres <julio@MacBook-Pro-de-Julio.local>
2022-10-24 22:20:30 -0300

    Correccion

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,12 +3,22 @@ require("mason").setup({
         icons = {
             package_installed = "?",
             package_pending = "?",
-            package_uninstalled = "?"
-        }
-    }
+            package_uninstalled = "?",
+        },
+    },
 })
-
 
 require("mason-lspconfig").setup({
-    ensure_installed = { "sumneko_lua", "rust_analyzer" }
+    ensure_installed = { "sumneko_lua", "rust_analyzer" },
 })
+
+--[[  
+:h mason-commands
+
+:Mason - opens a graphical status window
+:MasonInstall <package> ... - installs/reinstalls the provided packages
+:MasonUninstall <package> ... - uninstalls the provided packages
+:MasonUninstallAll - uninstalls all packages
+:MasonLog - opens the mason.nvim log file in a new tab window
+]]
+--
