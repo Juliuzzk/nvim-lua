d0deb39fe8e233299a498a46eacb51928ac97656
j.caceres.sanzana@gmail.com <j.caceres.sanzana@gmail.com>
2023-08-19 23:15:54 -0400

    Integracion con vscode neovim

--- init.lua
+++ init.lua
@@ -14,9 +14,14 @@ Minimalist and personal nvim config, based on LazyVim Config.
 -- Import Lua modules
 -----------------------------------------------------------
 
--- Settingsk
-require("core/settings") -- settings
--- Keymaps
-require("core/keymaps") -- keymaps
--- Lazy Init
-require("core/lazy")
+if vim.g.vscode then
+	--vim.cmd([[source $HOME/.config/nvim/vscode/settings.vim]])
+	vim.cmd([[source C:/Users/Julio/AppData/Local/nvim/vscode/settings.vim]])
+else
+	-- Settingsk
+	require("core/settings") -- settings
+	-- Keymaps
+	require("core/keymaps") -- keymaps
+	-- Lazy Init
+	require("core/lazy")
+end
