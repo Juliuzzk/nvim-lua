81e6d820a05ede8bccb2962f8edc839347ba9219
Julio Esteban Cáceres Sanzana <Julio@Macbook-M1-Pro.local>
2024-02-14 22:07:43 -0300

    Se descomenta custom menu

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,7 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- local cmd = vim.cmd -- execute Vim commands
+local cmd = vim.cmd -- execute Vim commands
 --
 -- -- -- Cambia internamente los archivos .json a .jsonc con el fin de poder desactivar diagnosticos de lsp en los commentarios
--- cmd([[autocmd BufNewFile,BufRead *.json setlocal filetype=jsonc]])
+cmd([[autocmd BufNewFile,BufRead *.json setlocal filetype=jsonc]])
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,100 +3,100 @@
 -- Add any additional keymaps here
 -----------------------------------------------------
 --
--- -- Keymaps configuration file: keymaps of neovim
--- -- and plugins.
--- -----------------------------------------------------------
+-- Keymaps configuration file: keymaps of neovim
+-- and plugins.
+-----------------------------------------------------------
+
+local map = vim.api.nvim_set_keymap
+local default_opts = { noremap = true, silent = true }
+
+-----------------------------------------------------------
+-- Neovim general keymaps:
 --
--- local map = vim.api.nvim_set_keymap
--- local default_opts = { noremap = true, silent = true }
---
--- -----------------------------------------------------------
--- -- Neovim general keymaps:
--- --
--- -----------------------------------------------------------
---
--- -- Move selected line / block of text in visual mode
--- map("x", "K", ":move '<-2<CR>gv-gv", default_opts)
--- map("x", "J", ":move '>+1<CR>gv-gv", default_opts)
---
--- -- Keep visual mode indenting
--- map("v", "<", "<gv", default_opts)
--- map("v", ">", ">gv", default_opts)
---
--- -- clear search highlighting
--- map("n", "<leader>fc", ":nohl <CR>", default_opts)
---
--- --Save
--- map("n", "<leader>ss", ":w<CR>", default_opts)
--- -- don't use arrow keys
--- --<nop>
--- map("", "<up>", "<up>", { noremap = true })
--- map("", "<down>", "<down>", { noremap = true })
--- map("", "<left>", "<left>", { noremap = true })
--- map("", "<right>", "<right>", { noremap = true })
---
--- map("i", "<C-h>", "<left>", default_opts)
--- map("i", "<C-j>", "<down>", default_opts)
--- map("i", "<C-k>", "<up>", default_opts)
--- map("i", "<C-l>", "<right>", default_opts)
---
--- -- move around splits using Ctrl + {h,j,k,l}
--- map("n", "<C-h>", "<C-w>h", default_opts)
--- map("n", "<C-j>", "<C-w>j", default_opts)
--- map("n", "<C-k>", "<C-w>k", default_opts)
--- map("n", "<C-l>", "<C-w>l", default_opts)
---
--- map("n", "<C-h>", "<C-w>h", default_opts)
--- map("n", "<C-j>", "<C-w>j", default_opts)
--- map("n", "<C-k>", "<C-w>k", default_opts)
--- map("n", "<C-l>", "<C-w>l", default_opts)
---
--- -- back tab
--- map("i", "<S-Tab>", "<C-d>", default_opts)
---
--- --" Select with shift + arrows
--- map("i", "<S-Left>", "<Left><C-o>v", default_opts)
--- map("i", "<S-Right>", "<C-o>v", default_opts)
--- map("i", "<S-Up>", "<Left><C-o>v<Up><Right>", default_opts)
--- map("i", "<S-Down>", "<C-o>v<Down><Left>", default_opts)
--- map("i", "<C-S-Left>", "<S-Left><C-Left>", {})
--- map("i", "<C-S-Right>", "<S-Right><C-Right>", {})
--- map("v", "<S-Left>", "<Left>", default_opts)
--- map("v", "<S-Right>", "<Right>", default_opts)
--- map("v", "<S-Up>", "<Up>", default_opts)
--- map("v", "<S-Down>", "<Down>", default_opts)
---
--- --" Auto unselect when not holding shift
--- map("v", "<Left>", "<Esc>", {})
--- map("v", "<Right>", "<Esc><Right>", {})
--- map("v", "<Up>", "<Esc><Up>", {})
--- map("v", "<Down>", "<Esc><Down>", {})
---
--- -- Move text up and down
--- --  A = shift
--- map("v", "<A-j>", ":m .+1<CR>==", default_opts)
--- map("v", "<A-k>", ":m .-2<CR>==", default_opts)
--- map("v", "p", '"_dP', default_opts)
--- map("x", "J", ":move '>+1<CR>gv-gv", default_opts)
--- map("x", "K", ":move '<-2<CR>gv-gv", default_opts)
--- map("x", "<A-j>", ":move '>+1<CR>gv-gv", default_opts)
--- map("x", "<A-k>", ":move '<-2<CR>gv-gv", default_opts)
---
--- -----------------------------------------------------------
--- -- Desactivar algunos keymaps:
--- -----------------------------------------------------------
--- --map("", "y", "<nop>", { noremap = true })
---
--- --map("", "J", "<nop>", { noremap = true })
--- --[[ map("", "q", "<nop>", { noremap = true }) -- recording ]]
---
--- vim.keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", {})
--- vim.keymap.set("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", {})
---
--- -- windows
--- map("n", "<leader>ww", "<C-W>p", { desc = "Other window" })
--- map("n", "<leader>wd", "<C-W>c", { desc = "Delete window" })
--- map("n", "<leader>w-", "<C-W>s", { desc = "Split window below" })
--- map("n", "<leader>w|", "<C-W>v", { desc = "Split window right" })
--- map("n", "<leader>-", "<C-W>s", { desc = "Split window below" })
--- map("n", "<leader>|", "<C-W>v", { desc = "Split window right" })
+-----------------------------------------------------------
+
+-- Move selected line / block of text in visual mode
+map("x", "K", ":move '<-2<CR>gv-gv", default_opts)
+map("x", "J", ":move '>+1<CR>gv-gv", default_opts)
+
+-- Keep visual mode indenting
+map("v", "<", "<gv", default_opts)
+map("v", ">", ">gv", default_opts)
+
+-- clear search highlighting
+map("n", "<leader>fc", ":nohl <CR>", default_opts)
+
+--Save
+map("n", "<leader>ss", ":w<CR>", default_opts)
+-- don't use arrow keys
+--<nop>
+map("", "<up>", "<up>", { noremap = true })
+map("", "<down>", "<down>", { noremap = true })
+map("", "<left>", "<left>", { noremap = true })
+map("", "<right>", "<right>", { noremap = true })
+
+map("i", "<C-h>", "<left>", default_opts)
+map("i", "<C-j>", "<down>", default_opts)
+map("i", "<C-k>", "<up>", default_opts)
+map("i", "<C-l>", "<right>", default_opts)
+
+-- move around splits using Ctrl + {h,j,k,l}
+map("n", "<C-h>", "<C-w>h", default_opts)
+map("n", "<C-j>", "<C-w>j", default_opts)
+map("n", "<C-k>", "<C-w>k", default_opts)
+map("n", "<C-l>", "<C-w>l", default_opts)
+
+map("n", "<C-h>", "<C-w>h", default_opts)
+map("n", "<C-j>", "<C-w>j", default_opts)
+map("n", "<C-k>", "<C-w>k", default_opts)
+map("n", "<C-l>", "<C-w>l", default_opts)
+
+-- back tab
+map("i", "<S-Tab>", "<C-d>", default_opts)
+
+--" Select with shift + arrows
+map("i", "<S-Left>", "<Left><C-o>v", default_opts)
+map("i", "<S-Right>", "<C-o>v", default_opts)
+map("i", "<S-Up>", "<Left><C-o>v<Up><Right>", default_opts)
+map("i", "<S-Down>", "<C-o>v<Down><Left>", default_opts)
+map("i", "<C-S-Left>", "<S-Left><C-Left>", {})
+map("i", "<C-S-Right>", "<S-Right><C-Right>", {})
+map("v", "<S-Left>", "<Left>", default_opts)
+map("v", "<S-Right>", "<Right>", default_opts)
+map("v", "<S-Up>", "<Up>", default_opts)
+map("v", "<S-Down>", "<Down>", default_opts)
+
+--" Auto unselect when not holding shift
+map("v", "<Left>", "<Esc>", {})
+map("v", "<Right>", "<Esc><Right>", {})
+map("v", "<Up>", "<Esc><Up>", {})
+map("v", "<Down>", "<Esc><Down>", {})
+
+-- Move text up and down
+--  A = shift
+map("v", "<A-j>", ":m .+1<CR>==", default_opts)
+map("v", "<A-k>", ":m .-2<CR>==", default_opts)
+map("v", "p", '"_dP', default_opts)
+map("x", "J", ":move '>+1<CR>gv-gv", default_opts)
+map("x", "K", ":move '<-2<CR>gv-gv", default_opts)
+map("x", "<A-j>", ":move '>+1<CR>gv-gv", default_opts)
+map("x", "<A-k>", ":move '<-2<CR>gv-gv", default_opts)
+
+-----------------------------------------------------------
+-- Desactivar algunos keymaps:
+-----------------------------------------------------------
+--map("", "y", "<nop>", { noremap = true })
+
+--map("", "J", "<nop>", { noremap = true })
+--[[ map("", "q", "<nop>", { noremap = true }) -- recording ]]
+
+vim.keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", {})
+vim.keymap.set("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", {})
+
+-- windows
+map("n", "<leader>ww", "<C-W>p", { desc = "Other window" })
+map("n", "<leader>wd", "<C-W>c", { desc = "Delete window" })
+map("n", "<leader>w-", "<C-W>s", { desc = "Split window below" })
+map("n", "<leader>w|", "<C-W>v", { desc = "Split window right" })
+map("n", "<leader>-", "<C-W>s", { desc = "Split window below" })
+map("n", "<leader>|", "<C-W>v", { desc = "Split window right" })
