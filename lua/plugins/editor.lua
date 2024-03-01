84e68598c9a2b16068638800689ae24d01b3cf14
Julio Esteban Cáceres Sanzana <Julio@Macbook-M1-Pro.local>
2024-03-01 00:05:54 -0300

    Se agrega vim visual multi

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -9,4 +9,20 @@ return {
       })
     end,
   },
+  {
+    "mg979/vim-visual-multi",
+    config = function()
+      vim.g.VM_default_mappings = 0
+      vim.g.VM_maps = {
+        ["Find Under"] = "<C-n>",
+        ["Find Subword Under"] = "<C->",
+        ["Select Cursor Down"] = "<M-Down>",
+        ["Select Cursor Up"] = "<M-Up>",
+      }
+
+      vim.keymap.set("n", "<C-x>", "<Plug>(VM-Find-Under)")
+      vim.keymap.set("n", "<M-Down>", "<Plug>(VM-Select-Cursor-Down)")
+      vim.keymap.set("n", "<M-Up>", "<Plug>(VM-Select-Cursor-Up)")
+    end,
+  },
 }
