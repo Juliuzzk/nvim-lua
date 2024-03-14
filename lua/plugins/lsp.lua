f1a6848015d07a93637920da705a19e95fe7f0fd
Julio Esteban Cáceres Sanzana <Julio@Macbook-M1-Pro.local>
2024-03-14 00:26:41 -0300

    Se crea funcion para deshabilitar algunos maps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,6 +3,18 @@ return {
   "neovim/nvim-lspconfig",
   init = function()
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    keys[#keys + 1] = { "<c-k>", false }
+
+    -- Función para buscar el índice del elemento que tiene "<c-k>"
+    local function findIndexForKey(keys, key)
+      for i, v in ipairs(keys) do
+        if v[1] == key then
+          return i
+        end
+      end
+      return nil
+    end
+
+    -- Reemplazar el elemento en el índice encontrado
+    keys[findIndexForKey(keys, "<c-k>")] = { "<c-k>", false }
   end,
 }
