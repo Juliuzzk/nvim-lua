f9a073a702407bb5cf0d93e7b56680c6b17ea312
Julio Esteban Cáceres Sanzana <Julio@Macbook-M1-Pro.local>
2024-09-22 13:36:35 -0300

    Addded custom map for obsidian notes

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -4,4 +4,20 @@ return {
         vim.api.nvim_command([[normal! p]])
         vim.fn.setreg("+", val)
     end, {}),
+
+    -- vim.keymap.set("n", "<leader>nrm", ":!rm '%:p'<cr>:bd<cr>"),
+    vim.keymap.set("n", "<leader>nrm", function()
+        -- Obtener la extensión del archivo actual
+        local file_ext = vim.fn.expand("%:e")
+
+        -- Comprobar si la extensión es 'md'
+        if file_ext == "md" then
+            -- Ejecutar el comando para eliminar el archivo y cerrar el buffer
+            vim.cmd("!rm '" .. vim.fn.expand("%:p") .. "'")
+            vim.cmd("bd")
+        else
+            -- Mensaje de advertencia si el archivo no es .md
+            print("This command is only for delete markdown files notes.")
+        end
+    end, { desc = "Delete .md files loaded in buffer" }),
 }
